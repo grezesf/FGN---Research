9147620ad6596f96b6cfc3c3176a51fd342b0378
Felix Grezes <grezesf@gmail.com>
2019-09-12 20:08:49 -0400

    feedforward net code with covar types

--- Finite_Gaussian_Network_lib/Feedforward_Classic_net.py
+++ Finite_Gaussian_Network_lib/Feedforward_Classic_net.py
@@ -8,40 +8,53 @@ import torch
 
 class Feedforward_Classic_net(nn.Module):
     
-    def __init__(self,in_feats, out_feats, hidden_l_nums, drop_p=0.0):
+    def __init__(self,in_feats, out_feats, hidden_layer_sizes, drop_p=0.0, **kwargs):
         super(Feedforward_Classic_net, self).__init__()
         
         # input dimension
         self.in_feats=in_feats
         # output imension (number of classes)
         self.out_feats=out_feats
+        # dropout prob (same throughout network)
+        self.drop_p = drop_p
+        # the number of neurons for each hidden layer (can be empty)
+#         hidden_layer_sizes
+        
         
         # the hidden layers
         # add modules
-        next_in = self.in_feats
         self.hidden_layers = nn.ModuleList([])
         
         # optional input dropout
         if drop_p > 0:
-            self.hidden_layers.append(nn.Dropout(p=drop_p)) 
-
-        for idx, next_out in enumerate(hidden_l_nums):
+            self.hidden_layers.append(nn.Dropout(p=self.drop_p)) 
+            
+        # input batchnorm
+        self.ib = nn.BatchNorm1d(self.in_feats)
+        
+        # add in the variable layers
+        next_in = self.in_feats
+        for idx, next_out in enumerate(hidden_layer_sizes):
             # linear layer
             self.hidden_layers.append(nn.Linear(next_in, next_out))
             # optional: batchnorm
             self.hidden_layers.append(nn.BatchNorm1d(next_out))
             # optional: dropout layer
             if drop_p > 0:
-                self.hidden_layers.append(nn.Dropout(p=drop_p))
+                self.hidden_layers.append(nn.Dropout(p=self.drop_p))
             # reset feat for next layer
             next_in = next_out
         
         # final layer
         self.fl = nn.Linear(next_in, self.out_feats)
+        # final layer batchnorm
+#         self.flb = nn.BatchNorm1d(self.out_feats)
         
     def forward(self, x):
         # squash the data
         x = x.view(-1, self.in_feats)
+        # input batchnorm
+        x = self.ib(x)
         # for each hidden layer
         for layer in self.hidden_layers:
             # apply layer (linear, batchnorm or dropout)
@@ -51,6 +64,8 @@ class Feedforward_Classic_net(nn.Module):
                 x = torch.tanh(x)
         # final out layer
         x= self.fl(x)
+        # final layer batchnorm
+#         x = self.flb(x)
         # NO softmax
 #         x = F.log_softmax(x, dim=-1)
         
--- Finite_Gaussian_Network_lib/Feedforward_FGN_net.py
+++ Finite_Gaussian_Network_lib/Feedforward_FGN_net.py
@@ -8,22 +8,25 @@ from FGN_layer import FGN_layer
 
 class Feedforward_FGN_net(nn.Module):
     
-    def __init__(self,in_feats, out_feats, hidden_l_nums, ordinal=2, drop_p=0.0, noisy_centers=False, random_eval=False):
+    def __init__(self, in_feats, out_feats, hidden_layer_sizes, drop_p=0.0, **kwargs):
         super(Feedforward_FGN_net, self).__init__()
         
         # input dimension
         self.in_feats=in_feats
         # output imension (number of classes)
         self.out_feats=out_feats
-        # ordinal used for the norm (distance to centers) computation
-        # (1=diamond, 2=euclidean, 3->float('inf')=approach manhattan)
-        self.ordinal = ordinal
         # dropout prob (same throughout network)
         self.drop_p = drop_p
-        # should noise be added to the centers during training?
-        self.noisy_centers = noisy_centers
-        # should the eval be random far from the neuron center?
-        self.random_eval = random_eval
+        # the number of neurons for each hidden layer (can be empty)
+#         hidden_layer_sizes
+        
+        ### kwargs for FGN_layer
+#         # covar_type: one of 'sphere', 'diag' or 'full'
+#         # ordinal: used for the norm (distance to centers) computation
+#         # (1=diamond, 2=euclidean, 3->float('inf')=approach manhattan) (only for sphere covar)
+#         # noisy_centers: should noise be added to the centers during training?
+#         # non_lin: should an extra non-linearity be used and if so which one?
+#         # can be False or a function on tensors such as torch.tanh()
         
         # the hidden layers
         # add modules
@@ -38,9 +41,9 @@ class Feedforward_FGN_net(nn.Module):
         
         # add in the variable layers
         next_in = self.in_feats
-        for idx, next_out in enumerate(hidden_l_nums):
+        for idx, next_out in enumerate(hidden_layer_sizes):
             # the FGN layer
-            self.hidden_layers.append(FGN_layer(next_in, next_out, ordinal=self.ordinal, noisy_centers=self.noisy_centers, random_eval=self.random_eval))
+            self.hidden_layers.append(FGN_layer(next_in, next_out, **kwargs))
             # optional: batchnorm
             self.hidden_layers.append(nn.BatchNorm1d(next_out))
             # optional: dropout layer
@@ -50,24 +53,33 @@ class Feedforward_FGN_net(nn.Module):
             next_in = next_out
 
         # final layer, always non-random eval, always non-noisy center (?)
-        self.fl = FGN_layer(next_in, self.out_feats, ordinal=self.ordinal, noisy_centers=False, random_eval=False)
+        self.fl = FGN_layer(next_in, self.out_feats, **kwargs)
         # final layer batchnorm
 #         self.flb = nn.BatchNorm1d(self.out_feats)
         
     def forward(self, x):
+        if (x != x).any(): raise TypeError("x 0 is nan \n {}".format(x))
         # squash the data
         x = x.view(-1, self.in_feats)
         # input batchnorm
         x = self.ib(x)
+        if (x != x).any():
+            raise TypeError("x 1 is nan \n {}".format(x))
         # for each hidden layer
-        for layer in self.hidden_layers:
+        for idx, layer in enumerate(self.hidden_layers):
             # apply layer (finite, batchnorm or dropout)
             x = layer(x)
+            if (x != x).any(): 
+                print("layer id:", idx)
+                raise TypeError("x 2 is nan \n {}".format(x))
             # if FGN_layer, apply non-linerarity (only needed to replicate classic net behavior)
 #             if isinstance(layer, FGN_layer):
 #                 x = torch.tanh(x)
         # final out layer
-        x= self.fl(x)
+        x = self.fl(x)
+        if (x != x).any():
+            print("final layer:", idx)
+            raise TypeError("x 3 is nan \n {}".format(x))
         # final layer batchnorm
 #         x = self.flb(x)
         # NO softmax
