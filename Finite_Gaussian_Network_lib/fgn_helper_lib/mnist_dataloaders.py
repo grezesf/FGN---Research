4d88b77504402424e7659fb9c7268fb410b3959f
Felix Grezes <grezesf@gmail.com>
2021-01-20 18:00:54 -0500

    highD viz and rand data bug (scale)

--- Finite_Gaussian_Network_lib/fgn_helper_lib/mnist_dataloaders.py
+++ Finite_Gaussian_Network_lib/fgn_helper_lib/mnist_dataloaders.py
@@ -14,7 +14,11 @@ def mnist_dataloaders(batch_size=32, batch_size_for_val=None, mnist_path='/home/
                        transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))
                 ])), 
             batch_size=batch_size, shuffle=True)
-
+    # notes: as is the data is PIL images, and transforms is only applied when batches are called.
+    # so original_mnist_train_loader.dataset.data[0] (raw data) is a PIL image (dtype=torch.uint8)
+    # but next(iter(original_mnist_train_loader))[0] (after transforms) is a tensor (dtype=torch.float32)
+    # ToTensor() essentially divides by 255 in this case
+    
     # split into train/validation
     train_ds =  original_mnist_train_loader.dataset.data[:50000, :, :]
     valid_ds =  original_mnist_train_loader.dataset.data[50000:, :, :]
@@ -24,16 +28,17 @@ def mnist_dataloaders(batch_size=32, batch_size_for_val=None, mnist_path='/home/
 
     # apply transforms
     # keep in mind you should only use the mean+std of data you've 'seen', the train data
-    train_ds=train_ds.float()/255.0
+    train_ds = train_ds.float()/255.0
     train_mean = train_ds.mean()
     train_std = train_ds.std()
     train_ds = transforms.Normalize(mean=(train_mean,), std=(train_std,))(train_ds)
-    valid_ds=valid_ds.float()/255.0
+    valid_ds = valid_ds.float()/255.0
     valid_ds = transforms.Normalize(mean=(train_mean,), std=(train_std,))(valid_ds)
 
     # recombine into dataloaders
     mnist_train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(train_ds, train_dst),
                                                      batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=False)
+    
     # batch_size for val and test should be much larger
     if batch_size_for_val==None:
         batch_size_for_val = 100*batch_size
@@ -47,12 +52,15 @@ def mnist_dataloaders(batch_size=32, batch_size_for_val=None, mnist_path='/home/
                       ), 
             batch_size=batch_size_for_val, shuffle=True)
     
-    # values used for other things
-    # minimum/maximum pixel value post normalization, from train dataset
-    min_pix = -0.4242129623889923095703125
-    max_pix =  2.8214867115020751953125000
-    # mean and std of train set
-    train_mean = -0.0000061691193877777550369501113891601562500000000
-    train_std = 0.999999344348907470703125000000
+#     # values used for other things
+#     pre-normalization means and std
+#     train_mean = 0.130961298942565917968750000000
+#     train_std = 0.308494895696640014648437500000
+#     # minimum/maximum pixel value post normalization, from train dataset
+#     min_pix = -0.4242129623889923095703125
+#     max_pix =  2.8214867115020751953125000
+#     # mean and std of train set post normalization
+#     train_mean = -0.0000061691193877777550369501113891601562500000000
+#     train_std = 0.999999344348907470703125000000
     
     return(mnist_train_loader, mnist_val_loader, mnist_test_loader)
--- Finite_Gaussian_Network_lib/fgn_helper_lib/mnist_random_dataloader.py
+++ Finite_Gaussian_Network_lib/fgn_helper_lib/mnist_random_dataloader.py
@@ -5,18 +5,20 @@ from torchvision import datasets, transforms
 def mnist_random_dataloader(num_samples=1000, batch_size=1):
 
     # create random 28*28 images
-
+    # pre-normalization means and std from train set
+    train_mean = 0.130961298942565917968750000000
+    train_std = 0.308494895696640014648437500000
+    
     # Random images dataset
-    x_rand = np.random.randint(low=0, high=255, size=(num_samples,28,28) )
+    x_rand = np.random.randint(low=0, high=255+1, size=(num_samples,28,28) )
     # convert to [0,1] and apply same transforms as for MNIST training
     x_rand = x_rand/255.0
     
-    # mean and std of MNIST train set
-    train_mean = -0.0000061691193877777550369501113891601562500000000
-    train_std = 0.999999344348907470703125000000
 
     x_rand = torch.Tensor(x_rand)
     x_rand = transforms.Normalize(mean=(train_mean,), std=(train_std,))(x_rand)
+    # note: because we normalize with the train set mean and std, this random data is NOT normalized to (0,1)
+    # this is expected. minpix and maxpix should be the same though
     rand_dataset = torch.utils.data.TensorDataset(x_rand)# create your dataset
     rand_dataloader = torch.utils.data.DataLoader(rand_dataset, 
                                                   batch_size=batch_size, 
